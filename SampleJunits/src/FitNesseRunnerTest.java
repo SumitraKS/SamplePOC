353f0e04c963e738432a73f80d7434e58e1b4580
SumitraKS <sumitra.k.s@gmail.com>
2015-09-06 18:10:30 +0530

    adding files

---
+++ SampleJunits/src/FitNesseRunnerTest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+
+import fitnesse.junit.FitNesseRunner;
+
+@RunWith(FitNesseRunner.class)
+@FitNesseRunner.Suite("*.txt")
+@FitNesseRunner.FitnesseDir(".")
+public class FitNesseRunnerTest {
+}
